e10a6f198af6c00ca3f102c82686cfecaa0f3198
sergioss911 <43449771+sergioss911@users.noreply.github.com>
2018-11-26 15:32:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var myImages =["https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjp2M24ifHeAhXcHzQIHUIMCh0QjRx6BAgBEAU&url=https%3A%2F%2Fthehappypuppysite.com%2Fshar-pei%2F&psig=AOvVaw11coACicb41XBqCFf7FM_F&ust=1543287789444636","https://cdn.akc.org/Marketplace/Breeds/Pembroke_Welsh_Corgi_SERP.jpg","https://www.card.com/sites/default/files/styles/card_gallery_image_medium/public/Card-DisseroBrands-14.jpg?itok=ammQy10m","https://global-free-classified-ads-s02.r.worldssl.net/user_images/1102139.jpg","https://st2.depositphotos.com/2892507/7444/i/950/depositphotos_74448903-stock-photo-husky-puppy.jpg"];
-
+var myImages =["http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Labrador-Retriever-1.jpg", "http://www.slate.com/content/dam/slate/articles/health_and_science/Science/2017/08/170803_MEDEX_SickDog.jpg.CROP.promo-xlarge2.jpg", "https://www.dogster.com/wp-content/uploads/2017/12/A-puppy-with-a-toy.jpg", "https://lovinlife.com/wp-content/uploads/2018/09/Dog.jpg", "https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/03/Pomeranian_01.jpeg"];
 
 var captionImages =["ChowChow","Corgi","BullDog","Yorki","Husky"];
 
