93d91eb8ad74173d7e6b91afa943e904da1b0d02
sergioss911 <43449771+sergioss911@users.noreply.github.com>
2018-11-26 15:35:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var myImages =["http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Labrador-Retriever-1.jpg", "http://www.slate.com/content/dam/slate/articles/health_and_science/Science/2017/08/170803_MEDEX_SickDog.jpg.CROP.promo-xlarge2.jpg", "https://www.dogster.com/wp-content/uploads/2017/12/A-puppy-with-a-toy.jpg", "https://lovinlife.com/wp-content/uploads/2018/09/Dog.jpg", "https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/03/Pomeranian_01.jpeg"];
+var myImages =["http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Labrador-Retriever-1.jpg","http://www.slate.com/content/dam/slate/articles/health_and_science/Science/2017/08/170803_MEDEX_SickDog.jpg.CROP.promo-xlarge2.jpg","https://www.dogster.com/wp-content/uploads/2017/12/A-puppy-with-a-toy.jpg","https://lovinlife.com/wp-content/uploads/2018/09/Dog.jpg","https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/03/Pomeranian_01.jpeg"];
 
 var captionImages =["ChowChow","Corgi","BullDog","Yorki","Husky"];
 
